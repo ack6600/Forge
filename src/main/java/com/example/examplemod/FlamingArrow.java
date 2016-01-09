f1430957541aebbbc983f313dc87988748388c16
ack6600 <adamurish@gmail.com>
2016-01-09 14:08:27 -0600

    Fixed

--- src/main/java/com/example/examplemod/FlamingArrow.java
+++ src/main/java/com/example/examplemod/FlamingArrow.java
@@ -1,13 +1,17 @@
 package com.example.examplemod;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityTNTPrimed;
 import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 
 public class FlamingArrow extends EntityArrow{
-
+	boolean b = false;
 	public FlamingArrow(World w,EntityLivingBase e,float f) 
 	{
 		super(w,e,f);
@@ -19,16 +23,27 @@ public class FlamingArrow extends EntityArrow{
 	@Override
 	public void onEntityUpdate() {
 		// TODO Auto-generated method stub
-		if(this.isCollided)
+		if(!b)
 		{
-			Entity e = new EntityTNTPrimed(this.getEntityWorld());
-			e.setPosition(this.getPosition().getX(), this.getPosition().getY(), this.getPosition().getZ());
-			if(this.getEntityWorld().isRemote)
+			BlockPos blockpos = new BlockPos(this.getPosition().getX(), this.getPosition().down().getY(), this.getPosition().getZ());
+		    IBlockState iblockstate = this.worldObj.getBlockState(blockpos);
+		    Block block = iblockstate.getBlock();
+			if(block.getMaterial() != Material.air)
 			{
+				System.out.println("hello");
+				Entity e = new EntityTNTPrimed(this.getEntityWorld());
+				e.setPosition(this.getPosition().getX(), this.getPosition().getY(), this.getPosition().getZ());
 				this.getEntityWorld().spawnEntityInWorld(e);
+				b = true;
 			}
-			
 		}
+//		if(this.isCollided)
+//		{
+//			Entity e = new EntityTNTPrimed(this.getEntityWorld());
+//			e.setPosition(this.getPosition().getX(), this.getPosition().getY(), this.getPosition().getZ());
+//				this.getEntityWorld().spawnEntityInWorld(e);
+//			
+//		}
 		super.onEntityUpdate();
 	}
 
